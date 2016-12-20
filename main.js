e8357ba124317c0f66883fd4f2e0eb553d72dfdd
Bill Brubaker <brubaker.bill@gmail.com>
2016-12-19 22:14:05 -0600

    adding global vars and stuff

--- main.js
+++ main.js
@@ -1,3 +1,20 @@
+var flags = [Boolean];
+var commands = [String];
+var functionStack = Array;
+
 process.argv.forEach((val, index) => {
   console.log(`${index}: ${val}`);
+  switch(val){
+      case "list":
+
+        break;
+  }
 });
+
+const defineFlag = (char, commandOrExecutionPath) => {
+
+}
+
+const defineFunction = (command, functionName, [parameters]) => {
+
+}
